aa68f4efe7f7b50153e5a918661460f39dfe105b
Micfan <micfan@users.noreply.github.com>
2013-12-09 23:13:28 +0800

    Add local database importment of `gemData`

--- script/rubygems.org.importdb.sh
+++ script/rubygems.org.importdb.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/env bash
 
+
 su postgres
+cd ~
+# TODO: write a Ruby spider to auto
 wget https://s3-us-west-2.amazonaws.com/rubygems-dumps/production/public_postgresql/2015.09.07.21.21.02/public_postgresql.tar
 
-# TODO: tar xvzf public_postgresql.tar
-sql_filename=PostgreSQL.sql
+tar -xvf public_postgresql.tar
+gunzip public_postgresql/databases/PostgreSQL.sql.gz
+
+rm -rf public_postgresql*
+
+sql_filename=public_postgresql/databases/PostgreSQL.sql
 
 dbname=gemData
 dropdb $dbname
 createdb $dbname
 psql $dbname -c 'create extension hstore;'
-psql $dbname < $sql_filepath
+
+psql $dbname < $sql_filename
+echo 'OK'
