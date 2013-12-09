3839fc4b1c36f313fac38c750b74ca86122ceba2
Micfan <micfan@users.noreply.github.com>
2013-12-09 23:13:28 +0800

    Parsed and enqueued packages in manifest.yml

---
+++ lib/parser/manifest_parser.rb
@@ -0,0 +1,49 @@
+require 'yaml'
+
+require_relative '../../lib/db'
+
+class ManifestParser
+  def initialize(repo_filepath, repo_id)
+    @repo_filepath = repo_filepath
+    @manifest_file_list = nil
+    ymls = api_get_repo_manifest_file_list(repo_id).values[0]
+    if ymls != nil
+      @manifest_file_list = ymls[0].gsub(' ', '').split(',')
+    end
+  end
+
+  def start
+    packs = []
+    @manifest_file_list.each {|file_name|
+      manifest_file_namepath = "#{@repo_filepath}/#{file_name}"
+      dependencies = ManifestYAML.new(manifest_file_namepath).get_dependencies
+      packs.concat(dependencies)
+    }
+    packs
+  end
+end
+
+class ManifestYAML
+  attr_reader :contents, :pack_language, :manifest_file_namepath
+
+  def initialize(manifest_file_namepath)
+    @contents = YAML.load_file(manifest_file_namepath)
+    @pack_language = 'manifest.yml'
+  end
+
+  def get_dependencies()
+    @contents['dependencies']
+  end
+
+  def get_exclude_files()
+    @contents['exclude_files']
+  end
+
+end
+
+if __FILE__ == $0
+  path = '/tmp/license_website/github.com/cloudfoundry/python-buildpack'
+  id = 81
+  p = ManifestParser.new(path, id)
+  p.start
+end
