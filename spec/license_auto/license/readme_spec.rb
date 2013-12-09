9ed28e5928dc5d866ba7db36af301dd749ff72ed
Micfan <micfan@users.noreply.github.com>
2013-12-09 23:13:28 +0800

    Add logger level

--- spec/license_auto/license/readme_spec.rb
+++ spec/license_auto/license/readme_spec.rb
@@ -3,10 +3,10 @@ require 'license_auto/package_manager'
 
 describe LicenseAuto::VirtualPackageManager do
   before do
-    stub_get(http_uri).
+    stub_get(readme_uri).
         to_return(:status => 200, :body => fixture(http_uri), :headers => {})
   end
 
   let(:file_uri) { 'spec/spec_helper.rb'}
-  let(:http_uri) { 'https://www.kernel.org/pub/site/README'}
+  let(:readme_uri) { 'https://www.kernel.org/pub/site/README'}
 end
