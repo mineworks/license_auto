70ca870612223ddddb089e14c43b38c9ba2ca591
CissyHao <cissy_hao@hotmail.com>
2013-12-09 23:13:28 +0800

    golang version unknown

--- script/apt-get.deps.sh
+++ script/apt-get.deps.sh
@@ -10,7 +10,7 @@ export license_auto_proxy='proxy.foo.com:8080'
 apt-get install -y rabbitmq-server \
     libpq-dev build-essential \
     libxml2 \
-    libxslt bzip2 cmake
+    libxslt bzip2 cmake gccgo-go
 
 # If mac osx
 # brew install icu4c
