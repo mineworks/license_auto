93de5bfdb31fb4b34f690b6a9fb35fbc4f6ba0e2
Micfan <micfan@users.noreply.github.com>
2013-12-09 23:13:28 +0800

    add launchpad.rb

--- lib/api/launchpad.rb
+++ lib/api/launchpad.rb
@@ -1,36 +1,53 @@
 require 'httparty'
-
 module API
-
   class Launchpad
-    def initialize b_name = '',b_version = ''
-      @binary_name = b_name
-      @binary_version = b_version
-      @launchpad_url = "https://launchpad.net/ubuntu/trusty/amd64/"
+    # DOC: https://launchpad.net/+apidoc/1.0.html
+    def initialize(distribution, distro_series, binary_package_name, binary_package_version, architecture='amd64')
+      @distribution = distribution
+      @distro_series = distro_series
+      @binary_package_name = binary_package_name
+      @binary_package_version = binary_package_version
+
+      @source_url = nil
+      @source_path = nil
+
     end
 
-    def launchpad(b_name = '',b_version = '')
-      name    = "libp11-kit0"
-      version = "0.20.2-2ubuntu2"
-      url = @launchpad_url + name + "/" + version
-      response = HTTParty.get(url)
-      p response.body
-      p "2222222222222222222"
-      a = response.body =~ /<dd id="source">/
-      p response.body[a,20]
-      puts response.body.class
+    def find_source_package_page_link()
+      # TODO: @Dragon add spider
+      source_package_link = nil
+    end
+
+    def find_source_code_download_url()
+      # TODO: @Dragon add spider
+    end
+
+    def download_source_code()
+      # TODO: @Micfan, uncompress
+      # TODO: configure a temp path to download and uncompress
+      source_code_path = nil
+    end
+
+    # Entry
+    def fetch_license_info_from_local_source(license_files_path)
+      # TODO: @Micfan, move it common lib:
     end
 
   end
-
-
 end
-
-
 
 if __FILE__ == $0
-  a = API::Launchpad.new
-  a.launchpad
+  distribution = 'ubuntu'
+  distro_series = 'trusty'
+  name = 'anacron'
+  version = '2.3-20ubuntu1'
+  archi
+  a = API::Launchpad.new(distribution, distro_series, name, version)
+  p a.find_source_package_page_link
 
+# ii  anacron                             2.3-20ubuntu1                    amd64        cron-like program that doesn't go by time
+# ii  apparmor                            2.8.95~2430-0ubuntu5.3           amd64        User-space parser utility for AppArmor
+# ii  apparmor-utils                      2.8.95~2430-0ubuntu5.3           amd64        Utilities for controlling AppArmor
+# ii  apt                                 1.0.1ubuntu2.10                  amd64        commandline package manager
+# ii  apt-utils                           1.0.1ubuntu2.10                  amd64        package management related utility programs
 end
-
