a3fa36e66934b03397ebb3f3df2924e7d34bc40e
CissyHao <cissy_hao@hotmail.com>
2015-09-23 16:02:10 +0800

    temp

--- lib/api/spider.rb
+++ lib/api/spider.rb
@@ -1,17 +1,40 @@
-require 'anemone'
+require "httparty"
 
 module API
 
   class Spider
-    def initialize(url)
+    def initialize(url, pack_name)
       @url = url
+      @pack_name = pack_name
     end
 
     def find_source_url()
-      Anemone.crawl(@url)
+      response = HTTParty.get(@url)
+      if response.code == 200
+        body = response.body
+        # TODO: author name valid
+        pattern = /(http[s]?:\/\/(github\.com|bitbucket\.org)\/|git@(github\.com|bitbucket\.org):)(?<author>.+?)\/#{@pack_name}/
+      
+        match_result = pattern.match(body)
+        author = match_result['author']
+        # p author
+        if author != nil
+          source_url = "https://github.com/#{author}/#{@pack_name}"
+          return source_url
+        end
+      else
+        # TODO: 404
+      end
     end
-
 
   end
 
 end ### API
+
+if __FILE__ == $0
+  url = 'http://johnnunemaker.com/httparty/'
+  pack_name = 'httparty'
+  s = API::Spider.new(url, pack_name)
+  source_url = s.find_source_url
+  p source_url
+end
