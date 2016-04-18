aa020361647fab85510b0f8c49dff0364dadc202
MichaelFan <fanlinsheng@gmail.com>
2016-04-18 17:13:14 +0800

    Modify a little rspec

--- spec/license_auto/repo_spec.rb
+++ spec/license_auto/repo_spec.rb
@@ -6,11 +6,10 @@ describe LicenseAuto::Repo do
     WebMock.disable!
   end
 
-  let(:gems_repo) { JSON.parse(File.read(fixture_path + '/' + 'my_repos/gems.json'))}
-  let(:license_auto_repo) { JSON.parse(File.read(fixture_path + '/' + 'my_repos/license_auto.json'))}
+  let(:license_auto_test_branch) { JSON.parse(File.read(fixture_path + '/' + 'my_repos/license_auto_test_branch.json'))}
 
   it "get dependencies of Repo: LicenseAuto" do
-    repo = LicenseAuto::Repo.new(license_auto_repo)
+    repo = LicenseAuto::Repo.new(license_auto_test_branch)
     dependencies = repo.find_dependencies
     expect(dependencies.empty?).to be(false)
     expect(dependencies["LicenseAuto::Bundler"].empty?).to be(false)
@@ -18,12 +17,4 @@ describe LicenseAuto::Repo do
 
     expect(dependencies["LicenseAuto::Npm"].first.fetch(:deps).empty?).to be(false)
   end
-
-  # TODO:
-  # it "get dependencies of Repo: Gems" do
-  #   repo = LicenseAuto::Repo.new(gems_repo)
-  #   dependencies = repo.find_dependencies
-  #   expect(dependencies.empty?).to be(false)
-  #   LicenseAuto.logger.debug(JSON.pretty_generate(dependencies))
-  # end
 end
